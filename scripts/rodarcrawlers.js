894162a51da1c9793502cc0b9f0285d3ad8c0edd
Cezar Novaes <cezarddt@gmail.com>
2025-02-08 16:27:23 -0300

    Separação melhores destinos
    Separação de melhores destinos do projeto passagens

--- scripts/rodarcrawlers.js
+++
@@ -1,7 +0,0 @@
-const { createController, runCrawler } = require("./crawlerbestdestinations")
-
-(() => {
-    console.log('INICIANDO')
-    const controller = createController()
-    runCrawler(controller)
-})()
